c6a416864b70b111d3f4a819ecdb1d2cd2d32c20
Hyu-Kim <hskim0000@gmail.com>
2022-11-11 14:22:52 -0500

    code updated Nov10 in debugging

--- data/gd.R
+++ data/gd.R
@@ -1,4 +1,4 @@
-# variables dependency on get_dist.R, data.R, smds.R
+# variables dependency on get_dist.R, data.R
 source("permanova_with_config.R")
 
 # MDS objective
@@ -15,6 +15,7 @@ mds_obj <- function(D, z){
 }
 
 
+# -----
 # MDS term
 gd_mds <- function(nit = 1000, eta = 1e-04, conv_lim = 1e-05,
                    D, S = 2, z0){
@@ -46,6 +47,7 @@ gd_mds <- function(nit = 1000, eta = 1e-04, conv_lim = 1e-05,
 }
 
 
+# -----
 # F statistic term, L1 norm
 gd_class <- function(nit = 1000, eta = 1e-04, z0, conv_lim = 1e-05,
                      D, S = 2, y){
@@ -88,8 +90,9 @@ gd_class <- function(nit = 1000, eta = 1e-04, z0, conv_lim = 1e-05,
 }
 
 
+# -----
 # MDS + F terms
-gd_cmds <- function(nit = 1000, eta = 1e-04, conv_lim = 1e-05, lambda = 500,
+gd_cmds <- function(nit = 1000, eta = 1e-04, conv_crit = 5e-03, lambda = 0.05,
                     z0, D, S = 2, y){
   # fixed step size (eta)
   N = dim(D)[1]
@@ -98,10 +101,12 @@ gd_cmds <- function(nit = 1000, eta = 1e-04, conv_lim = 1e-05, lambda = 500,
   Fz_cur <- pseudo_F(mat = z_cur, trt = y)$pseudoF
   F0 <- pseudo_F(d = D, trt = y)$pseudoF
   Finit <- Fz_cur
+  obj_total = mds_obj(D = D, z = z_cur) + lambda*abs(F0 - Fz_cur)
   for(t in 1:nit){
-    obj_mds = mds_obj(D = D, z = z_cur)
-    obj_total = obj_mds + lambda*abs(F0 - Fz_cur)
-    print(paste('iteration', t, 'total', obj_total, 'Fz', Fz_cur))
+    obj_total_prev <- obj_total
+    print(paste('iteration', t, 
+                '  total', sprintf(obj_total, fmt = '%#.3f'), 
+                '  Fz', sprintf(Fz_cur, fmt = '%#.3f')))
     for(i in 1:N){
       d_f <- rep(0, S)
       tmp11 <- tmp22 <- rep(0, S)
@@ -124,12 +129,11 @@ gd_cmds <- function(nit = 1000, eta = 1e-04, conv_lim = 1e-05, lambda = 500,
       z_cur[i,] <- z_cur[i,] - eta * (d_f + lambda * d_g)
       Fz_cur <- pseudo_F(mat = z_cur, trt = y)$pseudoF
     }
-    
-    # if(sqrt(sum((z_up - z_cur)^2)) < conv_lim){
-    #   print("Converged")
-    #   break
-    # ### convergence at every i?
-    # }
+    obj_total = mds_obj(D = D, z = z_cur) + lambda*abs(F0 - Fz_cur)
+    if(abs(obj_total - obj_total_prev) < conv_crit * obj_total){
+      print("Converged")
+      break
+      }
   }
   obj0 = mds_obj(D = D, z = z0)
   obj_up = mds_obj(D = D, z = z_cur)
@@ -150,7 +154,7 @@ plot(tmp$z, col = y2)
 tmp2 <- gd_class(nit = 2, z0 = zmds1, D = as.matrix(dist1), y = y1)
 plot(tmp2$z, col = y1)
 
-tmp3 <- gd_cmds(nit = 15, D = as.matrix(dist1), z0 = zmds1, y=y1, lambda = 0.05, eta = 5e-04)
+tmp3 <- gd_cmds(nit = 30, D = as.matrix(dist1), z0 = zmds1, y=y1, lambda = 0.05, eta = 1e-03)
 tmp3
 
 tmp4 <- gd_cmds(nit = 15, D = as.matrix(dist2), z0 = zmds2, y=y2, lambda = 0.05)
@@ -164,3 +168,17 @@ plot(tmp3$z, col = y1,
 plot(tmp4$z, col = y2,
      main = paste("F0=", round(tmp4$F0, 4), ", F_up=", round(tmp4$Fup, 4),
                   "Finit=", round(tmp4$Finit, 4)))
+
+
+# EXPORT
+wd <- '~/Google Drive/My Drive/Dimension reduction study/2022-11 iss5/results'
+sink(paste(wd, "gd_cmds_iter_inf_lamb_5e-2_eta_1e-3.txt", sep='/'))
+print(tmp4)
+sink()
+
+png(filename=paste(wd, "gd_cmds_iter_inf_lamb_5e-2_eta_1e-3.png", sep='/'))
+plot(tmp3$z, col = y1, 
+     main = paste("F0=", round(tmp4$F0, 4), 
+                  ", F_up=", round(tmp4$Fup, 4),
+                  "Finit=", round(tmp4$Finit, 4)))
+dev.off()
