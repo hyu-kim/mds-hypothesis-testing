0a134aca15af62894a5102c3cdddaaec301e8e95
Hyu-Kim <hskim0000@gmail.com>
2022-11-22 00:15:20 -0500

    gd_cmds explodes. Needs a fix somewhere.

--- data/gd.R
+++ data/gd.R
@@ -86,7 +86,7 @@ grad_conf_obj <- function(y, z, D){
   term1 <- sum((1 - (1+phi)*y_indmat) * z_distmat)
   term2 <- (1 - (1+phi)*y_ind_3d) * z_dist_3d # (N,N,S)
   term2 <- rowSums(aperm(term2, c(1,3,2)), dims=2) # (N,S)
-  res <- 8*(N-2) * res
+  res <- 8*(N-2) * term1 * term2
   return(res)
 }
 
@@ -113,16 +113,14 @@ gd_mds <- function(nit = 100, eta = 1e-04,
 
 # MDS + F terms
 gd_cmds <- function(nit = 100, eta = 1e-04, conv_crit = 5e-03, lambda = 0.05,
-                    z0, D, y){
-  # fixed step size (eta)
+                    z0, distmat, y){
   N <- dim(z0)[1]
   S <- dim(z0)[2]
-  a <- length(table(y))
   z_cur <- z0 #matrix, N * S
   Fz_cur <- pseudo_F(mat = z_cur, trt = y)$pseudoF
-  F0 <- pseudo_F(d = D, trt = y)$pseudoF
+  F0 <- pseudo_F(d = distmat, trt = y)$pseudoF
   Finit <- Fz_cur
-  obj_total = mds_obj(D = D, z = z_cur) + lambda*abs(F0 - Fz_cur)
+  obj_total = mds_obj(D = distmat, z = z_cur) + lambda * conf_obj(y=y, z=z_cur, D=distmat)
   for(t in 1:nit){
     obj_total_prev <- obj_total
     print(paste('iteration', t, 
@@ -130,36 +128,20 @@ gd_cmds <- function(nit = 100, eta = 1e-04, conv_crit = 5e-03, lambda = 0.05,
                 '  Fz', sprintf(Fz_cur, fmt = '%#.3f'),
                 '  F0', sprintf(F0, fmt = '%#.3f')
                 ))
-    for(i in 1:N){
-      d_f <- rep(0, S)
-      tmp11 <- tmp22 <- rep(0, S)
-      tmp12 <- tmp21 <- 0
-      for(j in 1:N){
-        if(j != i) {
-          d_f <- d_f + 2 * (1 - D[i,j]/sqrt(sum((z_cur[i,] - z_cur[j,])^2))) * 
-            (z_cur[i,] - z_cur[j,])
-        }
-        tmp11 <- tmp11 + z_cur[i,] - z_cur[j,]
-        tmp22 <- tmp22 + as.numeric(ifelse(y[i] == y[j], z_cur[i,] - z_cur[j,], rep(0, S)))
-        for(i_ in 1:N){
-          tmp21 <- tmp21 + sum((z_cur[i_,] - z_cur[j,])^2)
-          tmp12 <- tmp12 + ifelse(y[i_] == y[j], 
-                                  sum((z_cur[i_,] - z_cur[j,])^2), 0)
-        }
-      }
-      tmp3 <- tmp12^(-2)
-      d_g <- 4 * (N-a)/a * sign(Fz_cur - F0) * (tmp11 * tmp12 - tmp21 * tmp22) * tmp3
-      z_cur[i,] <- z_cur[i,] - eta * (d_f + lambda * d_g)
-      Fz_cur <- pseudo_F(mat = z_cur, trt = y)$pseudoF
-    }
-    obj_total = mds_obj(D = D, z = z_cur) + lambda*abs(F0 - Fz_cur)
+    #   d_g <- 4 * (N-a)/a * sign(Fz_cur - F0) * (tmp11 * tmp12 - tmp21 * tmp22) * tmp3
+    #   z_cur[i,] <- z_cur[i,] - eta * (d_f + lambda * d_g)
+    #   Fz_cur <- pseudo_F(mat = z_cur, trt = y)$pseudoF
+    # }
+    grad_total <- grad_mds_obj(D=distmat, z=z_cur) + lambda * grad_conf_obj(y=y, z=z_cur, D=distmat)
+    z_cur <- z_cur - eta * grad_total
+    obj_total = mds_obj(D = distmat, z = z_cur) + lambda * conf_obj(y=y, z=z_cur, D=distmat)
     if(abs(obj_total - obj_total_prev) < conv_crit * obj_total){
       print("Converged")
       break
       }
   }
-  obj0 = mds_obj(D = D, z = z0)
-  obj_up = mds_obj(D = D, z = z_cur)
+  obj0 = mds_obj(D = distmat, z = z0)
+  obj_up = mds_obj(D = distmat, z = z_cur)
   
   return(list(z = z_cur, obj0 = obj0, obj_up = obj_up,
               F0 = F0, Finit = Finit, Fup = Fz_cur))
@@ -174,13 +156,13 @@ tmp <- gd_mds(nit = 100, z0 = zmds1, distmat = as.matrix(dist1))
 tmp
 plot(tmp$z, col = y2)
 
-tmp2 <- gd_class(nit = 2, z0 = zmds1, D = as.matrix(dist1), y = y1)
-plot(tmp2$z, col = y1)
+# tmp2 <- gd_class(nit = 2, z0 = zmds1, D = as.matrix(dist1), y = y1)
+# plot(tmp2$z, col = y1)
 
-tmp3 <- gd_cmds(nit = 30, D = as.matrix(dist1), z0 = zmds1, y=y1, lambda = 0.05, eta = 1e-03)
+tmp3 <- gd_cmds(nit = 30, distmat = as.matrix(dist1), z0 = zmds1, y=y1, lambda = 0.05, eta = 1e-03)
 tmp3
 
-tmp4 <- gd_cmds(nit = 15, D = as.matrix(dist2), z0 = zmds2, y=y2, lambda = 0.05)
+tmp4 <- gd_cmds(nit = 15, distmat = as.matrix(dist2), z0 = zmds2, y=y2, lambda = 0.05)
 tmp4
 
 
