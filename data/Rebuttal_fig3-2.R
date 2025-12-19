518094eaa6dfe14356a23989c2c06567b2757ea6
Hyu Kim <hyungseok.kim@sanofi.com>
2025-12-19 17:25:48 -0500

    in works

--- data/Rebuttal_fig3-2.R
+++ data/Rebuttal_fig3-2.R
@@ -22,7 +22,7 @@ run_mm_cmds <- function(params) {
                  threshold = 0, dataset = 'sim_rev_1-N100', 
                  folder = "result/Revision2_Dec2025")
   
-  write.csv(res, sprintf('Simulated/F-MDS/Results/sim_rev_%d-N%d-fmds-%.2f-Z', r, N, l), row.names=FALSE)
+  # write.csv(res, sprintf('Simulated/F-MDS/Results/sim_rev_%d-N%d-fmds-%.2f-Z', r, N, l), row.names=FALSE)
   
   print(sprintf("MM Done. N: %d, Replicate: %d, For lambda: %g", N, r, l))
 }
@@ -44,3 +44,55 @@ clusterExport(cl, ls(envir = .GlobalEnv), envir = environment())
 results <- parLapply(cl, split(param_combinations, seq_len(nrow(param_combinations))), run_mm_cmds)
 
 stopCluster(cl)
+
+
+## Plot results
+log_all_df <- data.frame(matrix(nrow=0, ncol=6))
+colnames(log_all_df) <- c('dataset', 'size', 'lambda', 'epoch', 'p_z', 'p_0')
+
+for(r in c(1)){
+  for(N in c(100)){
+    for(l in c(0.2, 0.5, 0.8)){
+      log_df <- read.csv(sprintf('result/Revision2_Dec2025/sim_rev_%g-N%g-fmds-%.2f-log.csv', r, N, l))
+      log_all_df <- rbind(log_all_df, 
+                          data.frame(dataset = sprintf('sim_rev_%g', r), 
+                                     size = N, lambda = l, 
+                                     epoch = log_df$epoch, 
+                                     p_z = log_df$p_z, p_0 = log_df$p_0
+                          ))
+    }
+  }
+}
+
+ggplot(log_all_df) + 
+  geom_point(aes(x=epoch, y=p_z, shape=as.factor(lambda)), size=1.5) +
+  facet_wrap2(~size, scales = "free", nrow = 1) +
+  # facetted_pos_scales(
+  #   x = list(
+  #     scale_x_continuous(limits = c(0, 30)),
+  #     scale_x_continuous(limits = c(0, 20)),
+  #     scale_x_continuous(limits = c(0, 32)),
+  #     scale_x_continuous(limits = c(0, 42))
+  #   )
+  # ) +
+  scale_y_continuous(limits = c(0, 1), breaks=seq(0,1,0.25)) +
+  scale_shape_manual(values=c(21,22,24)) +
+  theme(strip.background = element_rect(fill=NA),
+        strip.text = element_blank(),
+        panel.background = element_rect(fill = "transparent", color = NA),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        plot.background = element_blank(),
+        panel.border = element_rect(fill = "transparent", color = 'black', size=0.5),
+        legend.position = "Bottom",
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.text.x = element_text(size=8, colour='black'),
+        # axis.text.y = element_text(size=8, colour='black'),
+        axis.text.y = element_blank(),
+        axis.line = element_blank(),
+        axis.ticks = element_line(linewidth=0.25, colour = 'black')
+  )
+
+ggsave('figures/fig2B_rev.pdf', width=4.8, height=1.35, units='in')
+
